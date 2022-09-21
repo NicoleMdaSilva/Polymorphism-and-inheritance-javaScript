2fe8bfcd9ac75d4eff90ffae86ee4bf6d1fd0321
PauloCMoreira <pauloskx9004@gmail.com>
2022-09-21 11:14:41 -0300

    update

--- build/script.js
+++ build/script.js
@@ -4,22 +4,27 @@ var Horse_1 = require("./Horse");
 var Dog_1 = require("./Dog");
 var Sloth_1 = require("./Sloth");
 function climb(animal) {
-    animal.climb;
+    animal.climb();
 }
 function Sounding(animal) {
-    animal.sound;
+    animal.sound();
 }
 function walking(animal) {
-    animal.walk;
+    animal.walk();
 }
 var horse = new Horse_1.Horse('Joaquin', 4);
+var dog = new Dog_1.Dog('Christoffer', 7);
+var sloth = new Sloth_1.Sloth('Flash', 3, true);
+console.log('------Horse------');
 console.log("The horse ".concat(horse.name, " is ").concat(horse.age, " years old."));
 Sounding(horse);
 walking(horse);
-var dog = new Dog_1.Dog('Christoffer', 7);
+console.log('------Dog------');
 console.log("The dog ".concat(dog.name, " is ").concat(dog.age, " years old."));
 Sounding(dog);
 walking(dog);
-var sloth = new Sloth_1.Sloth('Flash', 3, true);
+console.log('------Sloth------');
 console.log("The sloth ".concat(sloth.name, " is ").concat(sloth.age, " years old."));
 climb(sloth);
+Sounding(sloth);
+walking(sloth);
--- src/script.ts
+++ src/script.ts
@@ -4,31 +4,37 @@ import { Sloth } from "./Sloth";
 import { ICanWalk } from "./ICanWalk";
 import { IemmitSound } from "./IEmmitSound";
 import { IClimbTree } from "./IClimbTree";
+import { Animal } from "./Animal";
 
 function climb(animal: IClimbTree){
-    animal.climb
+    animal.climb();
 }
 function Sounding(animal: IemmitSound){
-    animal.sound
+    animal.sound();
 }
 function walking(animal: ICanWalk){
-    animal.walk
+    animal.walk();
 }
 
 const horse = new Horse('Joaquin', 4);
+const dog = new Dog('Christoffer', 7);
+const sloth = new Sloth('Flash', 3, true);
 
+console.log('------Horse------')
 console.log(`The horse ${horse.name} is ${horse.age} years old.`);
 Sounding(horse);
 walking(horse);
 
-const dog = new Dog('Christoffer', 7);
 
+console.log('------Dog------')
 console.log(`The dog ${dog.name} is ${dog.age} years old.`);
 Sounding(dog);
 walking(dog);
 
-const sloth = new Sloth('Flash', 3, true);
+console.log('------Sloth------')
 console.log(`The sloth ${sloth.name} is ${sloth.age} years old.`);
 climb(sloth);
+Sounding(sloth);
+walking(sloth);
 
 
